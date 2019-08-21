9eeb8fd5d8707fd25b943ca7590b34c3478361f5
Yash Fatnani <yash@jpctechnologies.net>
2019-08-21 11:16:22 +0530

    Added getFloatbotUserID method

--- app/src/main/java/sample/app/floatbot/SampleActivity.java
+++ app/src/main/java/sample/app/floatbot/SampleActivity.java
@@ -31,16 +31,30 @@ public class SampleActivity extends AppCompatActivity implements FloatbotFragmen
         bundle.putString("bot_id","5cc00545e6293668180a5d12");
         bundle.putString("key","U9vuk4FWthYO8ygE25nRqApbL1TSHx");
         bundle.putBoolean("showHeader",true);
-//      bundle.putString("push_token",<FCM TOKEN>);
-//      bundle.putString("data",<AES-CBC-256 Encrypted Json String>);
+        //bundle.putString("push_token",<FCM TOKEN>);
+        //bundle.putString("data",<AES-CBC-256 Encrypted Json String>);
 
         flb.setArguments(bundle);
         transaction.replace(R.id.addView,flb);
         transaction.commit();
-//        flb.showSessions();
-//      FloatbotUtils.setLoadingView(this,R.layout.connecting);
-//      FloatbotUtils.setNoInternetConnectionView(this,R.layout.nointernet);
-//      FloatbotUtils.clearUserData(this);
-//      FloatbotUtils.updatePushToken("<FCM TOKEN>");
+
+        //To show list of session to users in chatbot uncomment below function
+        //flb.showSessions();
+
+        /*Get floatbot user id from SDK*/
+        //FloatbotUtils.getFloatbotUserID(this);
+
+        /*Set Customized view for loading screen*/
+        //FloatbotUtils.setLoadingView(this,R.layout.connecting);
+
+        /* Set customized view for no internet connection*/
+        //FloatbotUtils.setNoInternetConnectionView(this,R.layout.nointernet);
+
+        /* To clear floatbot user data from app */
+        //FloatbotUtils.clearUserData(this);
+
+        /* To update token in floatbot, call below function with new push token */
+        //FloatbotUtils.updatePushToken("<FCM TOKEN>");
+
     }
 }
